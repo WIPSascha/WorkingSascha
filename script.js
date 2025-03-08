bd4a15c5fdc441ccb1c815417d625db0057f31d5
WIPSascha <sascharumuller@hotmail.com>
2025-03-08 17:11:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Pas deze variabelen aan
-let progress = 10; // Pas het percentage aan (0-100)
+let progress = 75; // Pas het percentage aan (0-100)
 let pageTitle = "Sascha 3D Coms"; // Paginatitel
 let mainTitle = "What is in progress?"; // Hoofdtitel
 let nextCommissionText = "Dino Chase - Koletee"; // Next commission text
